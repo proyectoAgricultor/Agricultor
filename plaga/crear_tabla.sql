7ac5c801b9dfa142c32037861a573e3abab530a3
Elioth Rivera <elioth010@elioth010.elioth.com>
2015-03-27 12:32:19 -0600

    agrego travis y modifco script de la tabla

--- plaga/crear_tabla.sql
+++ plaga/crear_tabla.sql
@@ -2,12 +2,24 @@
 
 
 /*creacion de la tabla para crear accesos  */
-create table clasificacion(
-	id int(15) auto_increment not null,
-	titulo varchar(100) not null,
-	estatus int(1) not null,
-	primary key(id)
-);
+CREATE TABLE IF NOT EXISTS `mydb`.`PLAGA` (
+  `ID` INT NOT NULL,
+  `NOMB_POPULAR` VARCHAR(45) NULL,
+  `NOMB_CIENTIFICO` VARCHAR(45) NULL,
+  `DESCRIPCION` VARCHAR(45) NULL,
+  `FUENTE` VARCHAR(45) NULL,
+  `USUARIO` VARCHAR(45) NULL,
+  `INGRESO` VARCHAR(45) NULL,
+  `STATUS` VARCHAR(45) NULL,
+  `TIPO_PLAGA_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`, `TIPO_PLAGA_ID`),
+  INDEX `fk_PLAGA_TIPO_PLAGA1_idx` (`TIPO_PLAGA_ID` ASC),
+  CONSTRAINT `fk_PLAGA_TIPO_PLAGA1`
+    FOREIGN KEY (`TIPO_PLAGA_ID`)
+    REFERENCES `mydb`.`TIPO_PLAGA` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 /*finaliza script si hay modulo de seguridad de solucionclic*/
 
